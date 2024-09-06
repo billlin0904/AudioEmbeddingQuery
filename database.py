61906302a106db4c82af909b731e606cc9099fd9
billlin0904 <billlin0904@gmail.com>
2024-09-06 15:35:26 +0800

    Add separate_audio and delete api

---
+++ database.py
@@ -0,0 +1,108 @@
+import librosa
+import numpy as np
+from chromadb import PersistentClient
+from chromadb.config import Settings
+from panns_inference import AudioTagging
+from typing import List
+from pydantic import BaseModel
+from typing import Union
+from fastapi import FastAPI, Query
+from separate import AudioSeparate
+import os
+
+class AudioEmbeddingDatabase:
+    def __init__(self, chroma_db_settings=None, collection_name="audio_embeddings", device='cuda'):
+        # 初始化 ChromaDB 客戶端和集合
+        self.client = PersistentClient(settings=chroma_db_settings)
+        self.collection = self.client.get_or_create_collection(collection_name)
+        self.at = AudioTagging(device=device)
+        self.separate = AudioSeparate(model_path="uvr5_weights/2_HP-UVR.pth", device=device)
+        self.collection_name = collection_name
+        
+    def __get_or_create_collection(self):
+        self.collection = self.client.get_or_create_collection(self.collection_name)
+
+    def separate_audio(self, file_path: str):
+        ins_root = "instrument"
+        name = os.path.basename(file_path)
+        self.separate.save_audio(file_path, ins_root = "instrument")
+        save_path = os.path.join(ins_root, 'instrument_{}.wav'.format(name)) 
+        return save_path
+
+    def embed_and_save(self, path, audio_id):
+        self.__get_or_create_collection()
+        
+        save_path = self.separate_audio(path)
+        print(f"Process {save_path}")
+        
+        # 讀取音頻文件並進行處理
+        audio, _ = librosa.core.load(save_path, sr=32000, mono=True)
+        audio = audio[None, :]  # 添加新的維度
+
+        try:
+            print(f"Get {save_path} embedding")
+            
+            # 獲取嵌入向量
+            _, embedding = self.at.inference(audio)
+            embedding = embedding / np.linalg.norm(embedding)  # 正規化嵌入向量
+            embedding = embedding.tolist()[0]  # 轉換為 Python 列表
+            
+            # 插入嵌入向量到 ChromaDB
+            self.collection.add(
+                documents=[path],
+                embeddings=[embedding],
+                metadatas=[{"audio_id": audio_id}],
+                ids = [audio_id]
+            )
+
+            print(f"Successfully inserted and saved path: {path} with ID: {audio_id}")
+        except Exception as e:
+            print(f"Failed: {path}; error: {e}")
+        finally:
+            os.remove(save_path)
+
+    def get_embeddings(self, paths):
+        # 對待檢索音頻批量抽取特徵，返回embedding
+        embedding_list = []
+        for x in paths:
+            save_path = self.separate_audio(x)
+            audio, _ = librosa.core.load(x, sr=32000, mono=True)
+            audio = audio[None, :]
+            try:
+                _, embedding = self.at.inference(audio)
+                embedding = embedding / np.linalg.norm(embedding)  # 正規化嵌入向量
+                embedding_list.append(embedding)
+            except Exception as e:
+                print(f"Embedding Failed: {x}, Error: {e}")
+            finally:
+                os.remove(save_path)
+        return np.array(embedding_list, dtype=np.float32).squeeze()
+
+    def query_embeddings(self, paths, n_results=5):
+        self.__get_or_create_collection()
+        
+        # 使用 Chroma 進行向量檢索並返回結果
+        embeddings = self.get_embeddings(paths).tolist()
+        res = []
+
+        try:
+            results = self.collection.query(
+                query_embeddings=embeddings,  # 查詢的嵌入向量
+                n_results=n_results,  # 每個查詢的返回結果數量限制
+                include=["documents", "distances", "metadatas"]  # 返回文檔和距離信息
+            )
+
+            # 處理檢索結果
+            for x in range(len(results["documents"])):
+                for i in range(len(results["documents"][x])):
+                    file_path = results["documents"][x][i]
+                    distances = results["distances"][x][i]
+                    metadata = results["metadatas"][x][i]
+                    audio_id = metadata["audio_id"]
+                    res.append((file_path, distances, audio_id))                
+        except Exception as e:
+            print("Failed to search vectors in Chroma: {}".format(e))
+        return res
+    
+    def flush(self):
+        self.client.reset()
--- embedding.py
+++ embedding.py
@@ -1,80 +1,14 @@
-import librosa
-import numpy as np
 from chromadb import PersistentClient
-from chromadb.config import Settings
-from panns_inference import AudioTagging
 from typing import List
 from pydantic import BaseModel
-from typing import Union
 from fastapi import FastAPI, Query
+import torch
+import os
+from database import AudioEmbeddingDatabase
+from separate import AudioSeparate
 
-class AudioEmbeddingDatabase:
-    def __init__(self, chroma_db_settings=None, collection_name="audio_embeddings", device='cpu'):
-        # 初始化 ChromaDB 客戶端和集合
-        self.client = PersistentClient(settings=chroma_db_settings)
-        self.collection = self.client.get_or_create_collection(collection_name)
-        self.at = AudioTagging(checkpoint_path=None, device=device)
-
-    def embed_and_save(self, path, audio_id):
-        print(f"Process {path}")
-        # 讀取音頻文件並進行處理
-        audio, _ = librosa.core.load(path, sr=32000, mono=True)
-        audio = audio[None, :]  # 添加新的維度
-
-        try:
-            # 獲取嵌入向量
-            _, embedding = self.at.inference(audio)
-            embedding = embedding / np.linalg.norm(embedding)  # 正規化嵌入向量
-            embedding = embedding.tolist()[0]  # 轉換為 Python 列表
-            
-            # 插入嵌入向量到 ChromaDB
-            self.collection.add(
-                documents=[path],
-                embeddings=[embedding],
-                metadatas=[{"audio_id": audio_id}],
-                ids = [audio_id]
-            )
-
-            print(f"Successfully inserted and saved path: {path} with ID: {audio_id}")
-        except Exception as e:
-            print(f"Failed: {path}; error: {e}")
-
-    def get_embeddings(self, paths):
-        # 對待檢索音頻批量抽取特徵，返回embedding
-        embedding_list = []
-        for x in paths:
-            audio, _ = librosa.core.load(x, sr=32000, mono=True)
-            audio = audio[None, :]
-            try:
-                _, embedding = self.at.inference(audio)
-                embedding = embedding / np.linalg.norm(embedding)  # 正規化嵌入向量
-                embedding_list.append(embedding)
-            except Exception as e:
-                print(f"Embedding Failed: {x}, Error: {e}")
-        return np.array(embedding_list, dtype=np.float32).squeeze()
-
-    def query_embeddings(self, paths, n_results=5):
-        # 使用 Chroma 進行向量檢索並返回結果
-        embeddings = self.get_embeddings(paths).tolist()
-        res = []
-
-        try:
-            results = self.collection.query(
-                query_embeddings=embeddings,  # 查詢的嵌入向量
-                n_results=n_results,  # 每個查詢的返回結果數量限制
-                include=["documents", "distances", "metadatas"]  # 返回文檔和距離信息
-            )
-
-            # 處理檢索結果
-            for x in range(len(results["documents"])):
-                file_path = results["documents"][x]  # 返回的結果文件列表
-                distances = results["distances"][x]  # 返回的距離列表
-                metadata = results["metadatas"][x]
-                audio_id = metadata[1]["audio_id"]
-                res.append((file_path, distances, audio_id))
-        except Exception as e:
-            print("Failed to search vectors in Chroma: {}".format(e))
-        return res
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f'Using device: {device}')
 
 app = FastAPI()
 db = AudioEmbeddingDatabase()
@@ -84,16 +18,29 @@ class EmbeddingItem(BaseModel):
     audio_id: str
 
 @app.post("/embed_and_save")
-async def embed_and_save(item: EmbeddingItem):
-    db.embed_and_save(item.file_path, item.audio_id)
+async def embed_and_save(item: EmbeddingItem):    
+    try:
+        db.embed_and_save(item.file_path, item.audio_id)
+    except Exception as e:
+        print(f"Failed: {item.file_path}; error: {e}")
 
 @app.get("/query_embeddings")
-async def query_embeddings(paths: List[str] = Query(None)):
+async def query_embeddings(paths: List[str] = Query(None)):    
     results = []
-    for result in db.query_embeddings(paths):
-        results.append({
-                "file_path": result[0], 
-                "distances": result[1], 
-                "audio_id": result[2]
-            })
+    try:
+        for result in db.query_embeddings(paths):
+            results.append({
+                    "file_path": result[0], 
+                    "distances": result[1], 
+                    "audio_id": result[2]
+                })
+    except Exception as e:
+        print(f"Error: {e}")
     return results
+
+@app.delete("/flushdb")
+async def flushdb():
+    try:
+        db.flush()
+    except Exception as e:
+        print(f"Error: {e}")
